01806629d4f7da9aa2fa247d67d6d50c23bffbb9
Aaron Palacios Olea <apalacios.dev@gmail.com>
2021-10-21 15:30:27 +0200

    2110211345

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { useRef } from "react";
 import  handleDownload  from '../helpers/handleDownload'
-import download_img from '../assets/images/download.png'
+import download_gif from '../assets/images/downloadGif.png'
 import code_img from '../assets/images/code.png'
-import like_img from '../assets/images/like.png'
 import '../assets/css/GifGridItem.css'
 import handleCopyCode from '../helpers/handleCopyCode'
 import { SnackBarAlert } from './SnackBarAlert';
@@ -24,7 +23,7 @@ const GifGridItem = ({ title, url, url_download }) => {
           <img src={url} alt={title} />
 
           <div className="info">
-            <img src={download_img}  onClick={() => handleDownload(url_download, title)} alt="Download Gif" />
+            <img src={download_gif}  onClick={() => handleDownload(url_download, title)} alt="Download Gif" />
             <img src={code_img} onClick={() => {handleCopyCode(url); snackbarRef.current.show()}} alt="Copy Code"/>       
           </div>
           
